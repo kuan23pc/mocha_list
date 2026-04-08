b98003129c004519b3e6da834efc01c0e8d2216c
Andjela Kuzmanovski <kuan23pc@student.ju.se>
2026-04-08 15:15:20 +0200

    Implemented loading and saving tasks from file - Andjela

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-# List to store all tasks
-tasks = []
+import json
+import os
+
+FILE_NAME = "tasks.json"
+
+def load_tasks():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r") as file:
+            return json.load(file)
+        return []
+
+tasks = load_tasks()
+
+with open(FILE_NAME, "w") as file:
+    json.dump(tasks, file, indent=4)
 
 # Function to add a new task
 def add_task():
     task = input("Enter task: ") 
     tasks.append(task)
+    save_tasks()
     print("Task added!")
+
+def save_tasks():
+    with open(FILE_NAME, "w") as file:
+        json.dump(tasks, file, indet=4)
 
 # Fun. to display all tasks
 def show_tasks():
