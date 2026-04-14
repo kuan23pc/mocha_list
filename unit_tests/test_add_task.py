877f3fefa743ab3b742f6eb34141bd47283ca33b
Marina Alramo <alma24wr@student.ju.se>
2026-04-14 10:48:10 +0200

    Add unit test for add task

---
+++ unit_tests/test_add_task.py
@@ -0,0 +1,22 @@
+import main
+
+def test_add_task(monkeypatch):
+    #Replace input() so it automatically returns "Test task"
+    monkeypatch.setattr("builtins.input", lambda _: "Test task")
+
+    #Disable saving to file to avoid modifying tasks.json
+    monkeypatch.setattr(main, "save_tasks", lambda: None)
+
+    # Reset the global tasks list before running the test
+    main.tasks = []
+
+    #Call the fun. we want to test
+    main.add_task()
+
+    #Verify that one task is added
+    assert len(main.tasks) == 1
+
+    #Verify the task content
+    assert main.tasks[0]["title"] == "Test task"
+    assert main.tasks[0]["completed"] is False
+    assert main.tasks[0]["deadline"] is None
