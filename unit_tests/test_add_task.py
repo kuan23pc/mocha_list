767b5ad8d101e2f7ee7e3f174ddd27d1567d27ae
Marina Alramo <alma24wr@student.ju.se>
2026-04-14 12:00:02 +0200

    Improve unit test with edge cases

--- unit_tests/test_add_task.py
+++ unit_tests/test_add_task.py
@@ -26,7 +26,10 @@ class TestAddTask(unittest.TestCase):
         main.save_tasks = self.original_save
         builtins.print = self.original_print
 
-    def test_add_task(self):
+    def test_add_valid_task(self):
+        #simulate user input
+        main.safe_input = lambda _: "Test task"
+
         #Run function
         main.add_task()
 
@@ -37,6 +40,24 @@ class TestAddTask(unittest.TestCase):
         self.assertEqual(main.tasks[0]["title"], "Test task")
         self.assertFalse(main.tasks[0]["completed"])
         self.assertIsNone(main.tasks[0]["deadline"])
+
+    def test_add_empty_task(self):
+        #simulate empty input
+        main.safe_input = lambda _: ""
+
+        main.add_task()
+
+        #no task should be added
+        self.assertEqual(len(main.tasks), 0)
+
+    def test_add_none_input(self):
+        #simulate cancelled input
+        main.safe_input = lambda _: None
+
+        main.add_task()
+
+        #No task should be added
+        self.assertEqual(len(main.tasks), 0)
 
 if __name__ == "__main__":
     unittest.main()
